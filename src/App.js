d2475166f8cf0c8825e051f8f547e50f1bdef762
ferthings <ferthings84@gmail.com>
2020-11-23 15:11:06 +0100

    prueba

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import Puzzle from './components/Puzzle';
 import image from './assets/main-square-urban-tourism-spain.jpg'
 
 function App() {
-  const onDone = () => {
-    console.log('SUCCESS');
+  const onComplete = () => {
+    console.log('Puzzle is completed!');
   }
 
   return (
@@ -12,7 +12,7 @@ function App() {
       width={575}
       height={800}
       pieces={3}
-      onComplete={onDone}
+      onComplete={onComplete}
     />
   );
 }
